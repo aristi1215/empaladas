a1582aa79dd52d6eb521b99394f090ad5ebfb88b
unknown <juan pablo aristizabal>
2022-03-31 17:18:52 -0500

    rappi.js

---
+++ rappi.js
@@ -0,0 +1,25 @@
+let boton = document.getElementById("boton")
+boton.addEventListener("click",escuchar )
+function escuchar(event){
+    event.preventDefault()
+    console.log("clicked");
+    let productos = document.getElementById("productos").value
+    let direccion = document.getElementById("direccion").value 
+    let correo = document.getElementById("correo").value
+    let cantidad = document.getElementById("cantidad").value
+
+    let salchipapasEespeciales = 12000
+    let pizzaPaisa = 17000
+    let perro = 7000 
+    let hamburguesa = 15000
+    let postreTiramisu = 9000
+    let postreTresLeches = 12000
+
+     
+
+
+
+
+
+
+}
